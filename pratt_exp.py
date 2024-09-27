7357dbe52cd06743ef197e6aaf0185258279f037
zofiaholubowska <zofijaholubowska@gmail.com>
2024-09-27 14:03:20 +0200

    experiment files

--- pratt_exp.py
+++ pratt_exp.py
@@ -6,44 +6,58 @@ import freefield
 import os
 import pickle
 from utils import shuffle_pairs
+from datetime import datetime
+import re
 
 # ========== DIRS AND PARAMS
-DIR = os.getcwd()
-STIM_DIR = f'{DIR}/stimuli'
-samplerate = 44828
+def load_parameters(subject):
+    DIR = os.getcwd()
+    STIM_DIR = f'{DIR}/stimuli'
+    samplerate = 44828
+    table = slab.ResultsTable(subject=subject,
+                              columns='timestamp, subject, task, idx, stim, frequency, direction, interval, azimuth, elevation')
+
+    return DIR, STIM_DIR, samplerate, table
+
 ### ========== ABSOLUTE MEASURES
 
 # =========== SET-UP
-
-proc_list = [['RX81', 'RX8', f'{DIR}/rcx/piano.rcx'],
+def load_processors(DIR):
+    proc_list = [['RX81', 'RX8', f'{DIR}/rcx/piano.rcx'],
                 ['RX82', 'RX8', f'{DIR}/rcx/piano.rcx'],
                  ['RP2', 'RP2', f'{DIR}/rcx/button.rcx']]
 
-freefield.initialize('dome', device=proc_list, sensor_tracking=True)
-freefield.set_logger('warning')
-directions = [21, 22, 23, 24, 25]
+    freefield.initialize('dome', device=proc_list, sensor_tracking=True)
+    freefield.set_logger('warning')
+    directions = [21, 22, 23, 24, 25]
 
-step = 5
+    return proc_list, directions
 
 # ========== STIMULI
-with open(f'{STIM_DIR}/freqs.pickle', 'rb') as fp:
-    range_freq = pickle.load(fp)
+def load_stimuli_absolute(STIM_DIR, subject):
+    step = 84
+    file_name = f'{STIM_DIR}/absolute_sequence/{subject}.pickle'
 
-pairs = [(sound, speaker) for sound in range_freq for speaker in directions for _ in range(3)]
-stims = shuffle_pairs(pairs)
-tone = slab.Sound.pinknoise(duration=0.1)
-freefield.set_signal_and_speaker(signal=tone, speaker=23, chan_tag='channel')
+    with open(file_name, 'rb') as f:
+        stims = pickle.load(f)
+    tone = slab.Sound.pinknoise(duration=0.1) # tone to confirm button press
+    freefield.set_signal_and_speaker(signal=tone, speaker=23, chan_tag='channel')
 
-def run_abs(subject, stims):
-    # TO DO RESULTS DF
-    print('##########\nCALIBRATION\n##########')
+    return step, tone, stims
+
+### ========== ABSOLUTE MEASURES
+def run_abs(subject, stims, samplerate, proc_list, table, step):
+    task = 'absolute'
+
+    print('#################\n## CALIBRATION ## \n#################')
     freefield.calibrate_sensor(led_feedback=True, button_control=True)  # sensor calibration
-    print('##########\nCALIBRATION COMPLETED\n##########')
+    print('#####################\nCALIBRATION COMPLETED\n#####################')
 
     input("Do you want to continue? (PRESS ENTER): ")
     print("Continuing...")
     stims_length = len(stims)
 
+    # ITERATE OVER ALL STIMULI
     for idx, stim in enumerate(stims):
         print(f'STIMULUS: {idx+1} / {stims_length}')
         frequency = stim[0]
@@ -77,10 +91,16 @@ def run_abs(subject, stims):
         freefield.write(tag='bitmask', value=0, processors='RX81')  # illuminate LED
         freefield.play()
         response = freefield.read('response', 'RP2', 0)
-
+        row = table.Row(timestamp=datetime.now(), subject = subject, task = task,
+                        idx = idx, stim = stim, frequency = frequency, direction = direction, interval= 'NA',
+                        azimuth = pose[0], elevation = pose[1])
+        table.write(row)
         time.sleep(1)
+        if (idx+1) == stims_length:
+            print('End of the first task.')
+            print('Take a break')
 
-        if (idx+1) % step == 0:
+        elif (idx+1) % step == 0:
             print('Time to take a break')
             input("Do you want to CALIBRATE? (PRESS ENTER): ")
 
@@ -93,22 +113,42 @@ def run_abs(subject, stims):
 
 
 
-### ========== INDUCED CONTEXT
+### ========== RELATIVE MEASURES
 
 # ========== STIMULI
 
-files = [f for f in os.listdir(STIM_DIR) if 'stim_' in f]
-random.shuffle(files)
+def load_stimuli_relative(STIM_DIR, directions):
 
-pairs = [(sound, speaker) for sound in files for speaker in directions]
-stims = shuffle_pairs(pairs)
+    files = [f for f in os.listdir(STIM_DIR) if 'stim_' in f]
+    random.shuffle(files)
 
-stims_length = len(stims)
+    pairs = [(sound, speaker) for sound in files for speaker in directions]
+    stims = shuffle_pairs(pairs)
+    tone = slab.Sound.pinknoise(duration=0.1)  # tone to confirm button press
+
+    step = 50
+
+    return step, stims, tone
 
 
-def run_rel(stims):
+def run_rel(subject, stims, STIM_DIR, samplerate, proc_list, step, table):
+
+    print('#################\n## CALIBRATION ## \n#################')
+    freefield.calibrate_sensor(led_feedback=True, button_control=True)  # sensor calibration
+    print('#####################\nCALIBRATION COMPLETED\n#####################')
+
+    input("Do you want to continue? (PRESS ENTER): ")
+    print("Continuing...")
+
+    stims_length = len(stims)
+    task = 'relative'
 
     for idx, stim in enumerate(stims):
+
+        task = 'absolute'
+        match = re.search(r'_(\-?\d+\.?\d*)\.csv$', stim[0])
+        if match:
+            interval = match.group(1)
 
         print(f'STIMULUS: {idx + 1} / {stims_length}')
         file_name = stim[0]
@@ -158,6 +198,25 @@ def run_rel(stims):
         response = freefield.read('response', 'RP2', 0)
 
         time.sleep(1)
+        row = table.Row(timestamp=datetime.now(), subject=subject, task=task,
+                        idx=idx, stim=stim, frequency=frequencies[i], direction=stim[1], interval = interval,
+                        azimuth=pose[0], elevation=pose[1])
+        table.write(row)
+
+        if (idx+1) == stims_length:
+            print('End of the second task.')
+            print('THANK YOU!')
+
+        elif (idx+1) % step == 0:
+            print('Time to take a break')
+            input("Do you want to CALIBRATE? (PRESS ENTER): ")
+
+            print('##########\nCALIBRATION\n##########')
+            freefield.calibrate_sensor(led_feedback=True, button_control=True)  # sensor calibration
+            print('##########\nCALIBRATION COMPLETED\n##########')
+
+            input("Do you want to continue? (PRESS ENTER): ")
+            print("Continuing...")
 
 
 
