a646de41c5c77070ed27e4edf56e893422d9fa10
suongwelp <suongwelp@gmail.com>
2025-02-10 14:36:07 +0100

    data collection

--- run_experiment.py
+++ run_experiment.py
@@ -2,7 +2,7 @@ from pratt_exp import load_parameters, load_processors, load_tones, run_pratt
 from utils import create_dataframe,plot_results_single_participant
 
 ### INSERT PARTICIPANT'S NUMBER HERE
-subject = 'sub13'
+subject = 'sub14'
 
 ### LOAD PARAMETERS
 DIR, STIM_DIR, RESULTS_DIR, PLOT_DIR, samplerate, table = load_parameters(subject)
@@ -12,6 +12,7 @@ step, shuffled_pairs, conditions = load_tones(STIM_DIR)
 
 ### LOAD PROCESSORS
 proc_list, directions = load_processors(DIR)
+
 
 ### RUN *EXPERIMENT 1* - absolute measures
 
